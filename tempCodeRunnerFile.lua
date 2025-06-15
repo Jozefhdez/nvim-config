a6b6482bade54c89097c15521ac8093b7e61c3a3
Jozef David Hernández Campos <jozefhdez2@gmail.com>
2025-06-15 00:14:56 -0700

    fix: delete some files

--- tempCodeRunnerFile.lua
+++
@@ -1,9 +0,0 @@
-  {
-  'nvim-tree/nvim-tree.lua',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    require('nvim-tree').setup()
-    -- Atajo para abrir/cerrar el explorador de archivos con <leader>§
-    vim.keymap.set('n', '<leader>§', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
-  end,
-},
